08f90b7d3cddabadc58d048e0f400a101244140a
himayatulmillah <hmillah211@gmail.com>
2021-02-23 10:14:08 +0700

    update image resource

--- getting_started_with_images.py
+++ getting_started_with_images.py
@@ -1,11 +1,11 @@
 import cv2
 
-img = cv2.imread('lena.jpg', 0)     # load image as array
+img = cv2.imread('data/lena.jpg', 0)    # load image as array
 
 print(img)
 
-cv2.imshow('image', img)            # show image window with 'image' title
-k = cv2.waitKey(0)                  # keep window showing the image
+cv2.imshow('image', img)                # show image window with 'image' title
+k = cv2.waitKey(0)                      # keep window showing the image
 
 if k == 27:
     cv2.destroyAllWindows()             # press esc button to destroy window
